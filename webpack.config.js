1c5f7fef849767aaf8d21464629403242211895f
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2016-06-17 13:39:12 +0200

    webpack: removed style, font, and image loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 
 var path = require('path')
 var webpack = require('webpack')
-var autoprefixer = require('autoprefixer')
 
 module.exports = {
   devtool: 'source-map',
@@ -16,40 +15,10 @@ module.exports = {
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel',
-    }, {
-      test: /\.scss$/,
-      loader: 'css!postcss!resolve-url!sass?sourceMap',
-    }, {
-      test: /\.css$/,
-      loader: 'css!postcss!resolve-url',
-    }, {
-      test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'url?limit=10000&mimetype=application/font-woff',
-    }, {
-      test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'url?limit=10000&mimetype=application/font-woff',
-    }, {
-      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'url?limit=10000&mimetype=application/octet-stream',
-    }, {
-      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'file',
-    }, {
-      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'url?limit=10000&mimetype=image/svg+xml',
-    }, {
-      test: /\.(jpe?g|png|gif|svg)$/i,
-      loaders: [
-        'file?hash=sha512&digest=hex&name=[hash].[ext]',
-        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false',
-      ],
     }],
   },
-  postcss: function () {
-    return [autoprefixer]
-  },
   resolve: {
-    extensions: ['', '.js', '.scss', '.css'],
+    extensions: ['', '.js'],
   },
   output: {
     path: path.join(__dirname, 'build'),
