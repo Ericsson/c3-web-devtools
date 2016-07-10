987f615dde2f283f1b0051a5fb0009496751db6f
Gustav Ung <guaschman@gmail.com>
2016-07-10 21:15:57 +0200

    src: Cleaned up the object view a bit

--- src/panel/RootView.js
+++ src/panel/RootView.js
@@ -4,10 +4,13 @@ import TreeView from './TreeView'
 
 let styles = {
   rootStyle: {
-    background: '#f00',
+    background: '#fff',
     width: '100%',
     height: '100%',
     oveflow: 'scroll'
+  },
+  treeStyle: {
+    paddingLeft: '2px',
   }
 }
 
@@ -33,12 +36,11 @@ class RootView extends React.Component {
   render() {
     let data = 'no data yet...'
     if (this.state.content) {
-      data = <TreeView data={JSON.parse(this.state.content)} depth={0} />
+      data = <TreeView data={JSON.parse(this.state.content)} depth={1} active={true} label={'root'} />
     }
     return (
       <div style={styles.rootStyle}>
-          <span>Hello react world!</span>
-          {data}
+          <div style={styles.treeStyle}>{data}</div>
       </div>
     )
   }
--- src/panel/TreeView.js
+++ src/panel/TreeView.js
@@ -3,36 +3,54 @@ import React from 'react'
 
 let styles = {
   rootStyle: {
-    paddingLeft: '2px'
+    paddingLeft: '2px',
+    cursor: 'pointer'
+  },
+  toggler: {
+    fontWeight: 'bold',
   }
 }
 
 class TreeView extends React.Component {
   constructor() {
     super()
+    this.state = {
+      active: false
+    }
+
+    this.clickHandler = this.clickHandler.bind(this)
+  }
+  clickHandler(event) {
+    event.preventDefault()
+    event.stopPropagation()
+    this.setState({active : !this.state.active})
   }
   render() {
     let depth = this.props.depth + 1
-    let data = 'no data yet... in a tree node!'
-    if (this.props.data) {
+    let data = ''
+    let props = this.props
+    styles.rootStyle.paddingLeft = (depth * 2) + 'px'
+    if (this.props.data && props.active || this.state.active) {
       let obj = this.props.data
       if (typeof obj === 'object') {
         data = Object.keys(obj).map((key) =>
-          <TreeView data={obj[key]} depth={depth}/>
+          <TreeView data={obj[key]} depth={depth} label={key}/>
         )
       } else if (typeof obj === 'array') {
+          styles.rootStyle.paddingLeft = ((depth + 1) * 2) + 'px'
         data = obj.map((key) =>
-          <div>{obj[key]}</div>
+          <div style={styles.rootStyle}>{obj[key]}</div>
         )
       } else {
-        data = <div>{this.props.data}</div>
+        styles.rootStyle.paddingLeft = ((depth + 1) * 2) + 'px'
+        data = <div style={styles.rootStyle}>{this.props.data}</div>
       }
     }
 
-    styles.rootStyle.paddingLeft = (depth * 2) + 'px'
-
     return (
-      <div style={styles.rootStyle}>
+      <div style={styles.rootStyle} >
+        <span style={styles.toggler} onClick={this.clickHandler}>{this.state.active ? 'less ' : 'more '}</span>
+        {props.label}
         {data}
       </div>
     )
