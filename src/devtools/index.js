0c6f61a250773b4b6ac9190117185e635a224fed
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2016-07-20 18:22:55 +0200

    devtools: add showing/hiding of panel based on polling

--- src/devtools/index.js
+++ src/devtools/index.js
@@ -39,30 +39,13 @@ port.onMessage.addListener(message => {
 })
 
 const messageHandlers = {
-  show(info) {
-    chrome.devtools.panels.create("C3 Web",
-      "icons/icon128.png",
-      "panel.html",
-      panel => {
-        log('showing panel')
-        panel.onShown.addListener(function(window) {
-          log('show panel')
-          requestData()
-        })
-        panel.onHidden.addListener(function() {
-          log('hide panel')
-        })
-      }
-    )
-  },
   init() {
-    log('Starting C3 SDK poll')
+    log('Starting SDK poll')
     startSdkPoll()
-    // TODO: cleanup panel
   }
 }
 
-var sdkPollIntervalId
+var sdkPollIntervalId = 0
 
 function startSdkPoll() {
   if (sdkPollIntervalId) {
@@ -71,17 +54,27 @@ function startSdkPoll() {
   sdkPollIntervalId = setInterval(() => {
     chrome.devtools.inspectedWindow.eval('!!window.__C3_SDK_INSTANCES__', (present, err) => {
       if (err) {
-        console.warn('Failed to poll C3 SDK presence: ' + err)
+        console.warn('Failed to poll SDK presence: ' + err)
       } else {
-        console.log('C3 SDK present: ' + present) // TODO: nope
+        console.log('SDK present: ' + present) // TODO: nope
         if (present) {
           clearInterval(sdkPollIntervalId)
-          onC3Found()
+          sdkPollIntervalId = 0
+          showPanel()
+          port.postMessage({type: 'sdkFound'})
         }
       }
     })
   }, POLL_INTERVAL)
 }
 
-function onC3Found() {
+var panelIsPresent = false
+
+function showPanel() {
+  if (panelIsPresent) {
+    port.postMessage({type: 'clearPanel'})
+  } else {
+    panelIsPresent = true
+    chrome.devtools.panels.create("C3 Web", "icons/icon128.png", "panel.html")
+  }
 }
